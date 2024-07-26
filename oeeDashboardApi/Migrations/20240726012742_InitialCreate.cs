5d4debf0064e2803fce3b29ff63b96ad8dae2e51
jordantargz <joaojordan.shell@gmail.com>
2024-07-25 22:39:23 -0300

    migrations geradas junto ao banco de dados - oee.db

---
+++ oeeDashboardApi/Migrations/20240726012742_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace oeeDashboardApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OEERecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Performance = table.Column<double>(type: "REAL", nullable: false),
+                    Quality = table.Column<double>(type: "REAL", nullable: false),
+                    Availability = table.Column<double>(type: "REAL", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OEERecords", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OEERecords");
+        }
+    }
+}
